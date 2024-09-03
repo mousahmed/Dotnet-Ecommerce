1806b5abf10924ac11bba7617c9d3eeec8c9aa12
Moustafa Ahmed <Mous.Ahmed@outlook.com>
2024-09-03 08:03:48 +0100

    Created Product List and Details in Customer Area Home page

--- Ecommerce/Areas/Customer/Controllers/HomeController.cs
+++ Ecommerce/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Ecommerce.DataAccess.Respository.IRepository;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,17 +9,23 @@ namespace EcommerceWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
-
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
