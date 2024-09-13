e468a61c5a848605a0c3db789220eccba136f971
Moustafa Ahmed <Mous.Ahmed@outlook.com>
2024-09-13 07:38:36 +0100

    Integration Stripe checkout

--- Ecommerce.DataAccess/Respository/IRepository/IOrderHeaderRepository.cs
+++ Ecommerce.DataAccess/Respository/IRepository/IOrderHeaderRepository.cs
@@ -10,5 +10,7 @@ namespace Ecommerce.DataAccess.Respository.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader obj);
+        void UpdateStatus(int id, string orderStatus, string? paymetStatus = null);
+        void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
     }
 }
--- Ecommerce.DataAccess/Respository/OrderHeaderRepository.cs
+++ Ecommerce.DataAccess/Respository/OrderHeaderRepository.cs
@@ -22,8 +22,36 @@ namespace Ecommerce.DataAccess.Respository
             _db.OrderHeaders.Update(obj);
         }
 
+        public void UpdateStatus(int id, string orderStatus, string? paymetStatus = null)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymetStatus))
+                {
+                    orderFromDb.PaymentStatus = paymetStatus;
+                }
+            }
 
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                if (!string.IsNullOrEmpty(sessionId))
+                {
+                    orderFromDb.SessionId = sessionId;
+                }
+                if (!string.IsNullOrEmpty(paymentIntentId))
+                {
+                    orderFromDb.PaymentIntentId = paymentIntentId;
+                    orderFromDb.PaymentDate = DateTime.Now;
+                }
+            }
+        }
 
     }
-
 }
